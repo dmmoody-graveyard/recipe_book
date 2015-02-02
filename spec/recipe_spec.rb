f9d16787e42fe07bdd3f3bed05eb6b66b895ed6c
Duane M. Moody <dmmoody@gmail.com>
2015-02-02 15:31:57 -0800

    add validation and titlecase spec

--- spec/recipe_spec.rb
+++ spec/recipe_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe Recipe do
-  
+  it('validates presence of name') do
+    recipe = Recipe.new({:name => ""})
+    expect(recipe.save()).to(eq(false))
+  end
+
+  it('takes a name and makes it titlecase') do
+    recipe = Recipe.create({:name => 'potato'})
+    expect(recipe.name()).to(eq('Potato'))
+  end
 end
