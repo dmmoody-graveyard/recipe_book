ad0589da1addab6f91bd98003a3817fbb398e998
Duane M. Moody <dmmoody@gmail.com>
2015-02-02 11:24:50 -0800

    add recipe route

--- app.rb
+++ app.rb
@@ -4,5 +4,13 @@ Bundler.require(:default)
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 
 get '/' do
+  @recipes = Recipe.all
   erb(:index)
+end
+
+post '/recipes' do
+  name = params.fetch 'recipe'
+  @recipe = Recipe.create(name: name)
+  @recipes = Recipe.all
+  redirect '/'
 end
