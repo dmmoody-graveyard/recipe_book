d7b5039fd0b93f90c768af790620875e446ac90e
Duane M. Moody <dmmoody@gmail.com>
2015-02-02 15:37:17 -0800

    add #titlecase

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -1,3 +1,13 @@
 class Recipe < ActiveRecord::Base
-  
+  validates(:name, :presence => true)
+  before_save(:title_case)
+
+private
+
+  define_method(:title_case) do
+    if self.name != self.name.upcase
+      self.name = (name().split(/(\W)/).map(&:capitalize).join)
+    end
+  end
+
 end
