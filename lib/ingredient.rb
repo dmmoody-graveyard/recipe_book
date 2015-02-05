613ac0f5382673404b41c00bb6afceeb786c1b10
Duane M. Moody <dmmoody@gmail.com>
2015-02-05 10:29:53 -0800

    add name validation => true

--- lib/ingredient.rb
+++ lib/ingredient.rb
@@ -2,4 +2,6 @@ class Ingredient < ActiveRecord::Base
   has_and_belongs_to_many :recipes
   has_one :ingredients_recipes
   has_one :measurements, :through => 'ingredients_recipes'
+
+  validates :name, :presence => true 
 end
--- spec/ingredient_spec.rb
+++ spec/ingredient_spec.rb
@@ -4,4 +4,9 @@ describe Ingredient do
   it{ should have_and_belong_to_many :recipes }
   it{ should have_one :ingredients_recipes }
   it{ should have_one :measurements }
+
+  it('validates presence of name') do
+    ingredient = Ingredient.new(:name => "")
+    expect(ingredient.save()).to eq(false)
+  end
 end
