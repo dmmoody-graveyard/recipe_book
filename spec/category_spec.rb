10f948ea4a97f35acb41bc0814d84e01fab1af56
Duane M. Moody <dmmoody@gmail.com>
2015-02-05 10:32:26 -0800

    add validates presence of name

--- spec/category_spec.rb
+++ spec/category_spec.rb
@@ -2,4 +2,9 @@ require 'spec_helper'
 
 describe Category do
   it { should have_and_belong_to_many :recipes }
+
+  it('validates presence of name') do
+    category = Category.new(:name => "")
+    expect(category.save).to eq(false)
+  end
 end
