ba9a1e4f3db9e301fb77e02650f5a49e83c4e782
Duane M. Moody <dmmoody@gmail.com>
2015-02-05 10:35:47 -0800

    add titlecase category name

--- lib/category.rb
+++ lib/category.rb
@@ -1,4 +1,14 @@
 class Category < ActiveRecord::Base
   has_and_belongs_to_many :recipes
   validates :name, :presence => true
+  before_save(:title_case)
+
+private
+
+  define_method(:title_case) do
+    if self.name != self.name.upcase
+      self.name = (name().split(/(\W)/).map(&:capitalize).join)
+    end
+  end
+
 end
--- spec/category_spec.rb
+++ spec/category_spec.rb
@@ -7,4 +7,9 @@ describe Category do
     category = Category.new(:name => "")
     expect(category.save).to eq(false)
   end
+
+  it('takes a category name and capitalizes it') do
+    category = Category.create :name => "mexican"
+    expect(category.name).to eq("Mexican")
+  end
 end
